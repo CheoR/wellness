183d1cafbbd1d391d60d59233b075f2fab2a4914
CheoR <cheo.roman+github@gmail.com>
2023-06-29 16:24:57 -0500

    build(Day): Datasource

---
+++ app/src/main/java/com/example/wellness/data/Datasource.kt
@@ -0,0 +1,158 @@
+package com.example.wellness.data
+
+import androidx.compose.ui.res.stringResource
+import com.example.wellness.R
+import com.example.wellness.model.Day
+
+class Datasource() {
+    fun loadDays(): List<Day> {
+        return listOf<Day>(
+            Day(
+                stringResourceId = R.string.day_1,
+                imageResourceId = R.drawable.unsplash_1,
+                descriptionResourceId = R.string.description_1,
+            ),
+            Day(
+                stringResourceId = R.string.day_2,
+                imageResourceId = R.drawable.unsplash_2,
+                descriptionResourceId = R.string.description_2,
+            ),
+            Day(
+                stringResourceId = R.string.day_3,
+                imageResourceId = R.drawable.unsplash_3,
+                descriptionResourceId = R.string.description_3,
+            ),
+            Day(
+                stringResourceId = R.string.day_4,
+                imageResourceId = R.drawable.unsplash_4,
+                descriptionResourceId = R.string.description_4,
+            ),
+            Day(
+                stringResourceId = R.string.day_5,
+                imageResourceId = R.drawable.unsplash_5,
+                descriptionResourceId = R.string.description_5,
+            ),
+            Day(
+                stringResourceId = R.string.day_6,
+                imageResourceId = R.drawable.unsplash_6,
+                descriptionResourceId = R.string.description_6,
+            ),
+            Day(
+                stringResourceId = R.string.day_7,
+                imageResourceId = R.drawable.unsplash_7,
+                descriptionResourceId = R.string.description_7,
+            ),
+            Day(
+                stringResourceId = R.string.day_8,
+                imageResourceId = R.drawable.unsplash_8,
+                descriptionResourceId = R.string.description_8,
+           ),
+            Day(
+                stringResourceId = R.string.day_9,
+                imageResourceId = R.drawable.unsplash_9,
+                descriptionResourceId = R.string.description_9,
+            ),
+            Day(
+                stringResourceId = R.string.day_10,
+                imageResourceId = R.drawable.unsplash_10,
+                descriptionResourceId = R.string.description_10,
+            ),
+            Day(
+                stringResourceId = R.string.day_11,
+                imageResourceId = R.drawable.unsplash_11,
+                descriptionResourceId = R.string.description_11,
+            ),
+            Day(
+                stringResourceId = R.string.day_12,
+                imageResourceId = R.drawable.unsplash_12,
+                descriptionResourceId = R.string.description_12,
+            ),
+            Day(
+                stringResourceId = R.string.day_13,
+                imageResourceId = R.drawable.unsplash_13,
+                descriptionResourceId = R.string.description_13,
+            ),
+            Day(
+                stringResourceId = R.string.day_14,
+                imageResourceId = R.drawable.unsplash_14,
+                descriptionResourceId = R.string.description_14,
+            ),
+            Day(
+                stringResourceId = R.string.day_15,
+                imageResourceId = R.drawable.unsplash_15,
+                descriptionResourceId = R.string.description_15,
+            ),
+            Day(
+                stringResourceId = R.string.day_16,
+                imageResourceId = R.drawable.unsplash_16,
+                descriptionResourceId = R.string.description_16,
+            ),
+            Day(
+                stringResourceId = R.string.day_17,
+                imageResourceId = R.drawable.unsplash_17,
+                descriptionResourceId = R.string.description_17,
+            ),
+            Day(
+                stringResourceId = R.string.day_18,
+                imageResourceId = R.drawable.unsplash_18,
+                descriptionResourceId = R.string.description_18,
+            ),
+            Day(
+                stringResourceId = R.string.day_19,
+                imageResourceId = R.drawable.unsplash_19,
+                descriptionResourceId = R.string.description_19,
+            ),
+            Day(
+                stringResourceId = R.string.day_20,
+                imageResourceId = R.drawable.unsplash_21,
+                descriptionResourceId = R.string.description_22,
+            ),
+            Day(
+                stringResourceId = R.string.day_23,
+                imageResourceId = R.drawable.unsplash_23,
+                descriptionResourceId = R.string.description_23,
+            ),
+            Day(
+                stringResourceId = R.string.day_24,
+                imageResourceId = R.drawable.unsplash_24,
+                descriptionResourceId = R.string.description_24,
+            ),
+            Day(
+                stringResourceId = R.string.day_25,
+                imageResourceId = R.drawable.unsplash_25,
+                descriptionResourceId = R.string.description_25,
+            ),
+            Day(
+                stringResourceId = R.string.day_26,
+                imageResourceId = R.drawable.unsplash_26,
+                descriptionResourceId = R.string.description_26,
+            ),
+            Day(
+                stringResourceId = R.string.day_27,
+                imageResourceId = R.drawable.unsplash_27,
+                descriptionResourceId = R.string.description_27,
+            ),
+            Day(
+                stringResourceId = R.string.day_28,
+                imageResourceId = R.drawable.unsplash_28,
+                descriptionResourceId = R.string.description_28,
+            ),
+            Day(
+                stringResourceId = R.string.day_29,
+                imageResourceId = R.drawable.unsplash_29,
+                descriptionResourceId = R.string.description_29,
+            ),
+            Day(
+                stringResourceId = R.string.day_30,
+                imageResourceId = R.drawable.unsplash_30,
+                descriptionResourceId = R.string.description_30,
+            ),
+            Day(
+                stringResourceId = R.string.day_31,
+                imageResourceId = R.drawable.unsplash_31,
+                descriptionResourceId = R.string.description_31,
+            ),
+        )
+    }
+}
+
