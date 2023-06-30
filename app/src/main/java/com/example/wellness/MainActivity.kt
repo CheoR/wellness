8fd288b0e6be5b64d9298868ec5f10dc5375ce8a
CheoR <cheo.roman+github@gmail.com>
2023-06-30 11:34:50 -0500

    build(Day): Card

--- app/src/main/java/com/example/wellness/MainActivity.kt
+++ app/src/main/java/com/example/wellness/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
@@ -28,6 +29,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.wellness.model.Day
 import com.example.wellness.ui.theme.WellnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +42,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DayCard()
+                    DayCard(
+                        Day(
+                            R.string.day_3,
+                            R.string.description_3,
+                            R.drawable.unsplash_3,
+                        )
+                    )
                 }
             }
         }
@@ -48,7 +56,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DayCard(modifier: Modifier = Modifier) {
+fun DayCard(day: Day, modifier: Modifier = Modifier) {
     // TODO: figure out why img size causes error when img size
     // for other prjoects are bigger than these images
     val image = painterResource(R.drawable.ic_launcher_foreground)
@@ -63,57 +71,85 @@ fun DayCard(modifier: Modifier = Modifier) {
             )
 
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-//                .background(color = Color.Red)
-                .padding(16.dp),
-        ){
-            Column(
+        Column() {
+            Text(
+                text = "Day #",
+                modifier = Modifier.padding(
+                    horizontal = 16.dp,
+                ),
+                style = MaterialTheme.typography.bodySmall,
+            )
+            Text(
+                text = stringResource(day.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.bodySmall,
+            )
+            Image(
+                painter = painterResource(day.imageResourceId),
+                contentDescription = stringResource(day.descriptionResourceId),
                 modifier = Modifier
                     .fillMaxWidth()
-//                    .background(color=Color.Green)
-            ) {
-                Text(
-                    text = "Day 1",
-                    style = MaterialTheme.typography.labelSmall,
-                    modifier = modifier
-                        .fillMaxWidth()
-//                        .background(color = Color.Red)
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(day.descriptionResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.bodySmall,
+            )
 
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                )
-                Text(
-                    text = "Do this",
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = modifier
-                        .fillMaxWidth()
-//                        .background(color = Color.Yellow)
-
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                )
-                Spacer(Modifier.width(16.dp))
-                Box(modifier=modifier) {
-                    Image(
-                        painter = image,
-                        contentDescription = imgDescription,
-                        contentScale = ContentScale.FillWidth,
-                        modifier = Modifier
-                            .clip(MaterialTheme.shapes.small),
-                    )
-                }
-                Spacer(Modifier.width(16.dp))
-                Text(
-                    text = "It is good for your health because",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = modifier
-                        .fillMaxWidth()
-//                        .background(color = Color.Black)
-
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                )
-            }
         }
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+////                .background(color = Color.Red)
+//                .padding(16.dp),
+//        ){
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+////                    .background(color=Color.Green)
+//            ) {
+//                Text(
+//                    text = "Day 1",
+//                    style = MaterialTheme.typography.labelSmall,
+//                    modifier = modifier
+//                        .fillMaxWidth()
+////                        .background(color = Color.Red)
+//
+//                        .padding(dimensionResource(R.dimen.padding_small)),
+//                )
+//                Text(
+//                    text = "Do this",
+//                    style = MaterialTheme.typography.headlineSmall,
+//                    modifier = modifier
+//                        .fillMaxWidth()
+////                        .background(color = Color.Yellow)
+//
+//                        .padding(dimensionResource(R.dimen.padding_small)),
+//                )
+//                Spacer(Modifier.width(16.dp))
+//                Box(modifier=modifier) {
+//                    Image(
+//                        painter = image,
+//                        contentDescription = imgDescription,
+//                        contentScale = ContentScale.FillWidth,
+//                        modifier = Modifier
+//                            .clip(MaterialTheme.shapes.small),
+//                    )
+//                }
+//                Spacer(Modifier.width(16.dp))
+//                Text(
+//                    text = "It is good for your health because",
+//                    style = MaterialTheme.typography.bodyMedium,
+//                    modifier = modifier
+//                        .fillMaxWidth()
+////                        .background(color = Color.Black)
+//
+//                        .padding(dimensionResource(R.dimen.padding_small)),
+//                )
+//            }
+//        }
 
     }
 }
@@ -122,7 +158,13 @@ fun DayCard(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WellnessTheme(darkTheme = false) {
-        DayCard()
+        DayCard(
+            Day(
+                R.string.day_1,
+                R.string.description_1,
+                R.drawable.unsplash_1,
+            )
+        )
     }
 }
 
@@ -130,6 +172,12 @@ fun GreetingPreview() {
 @Composable
 fun GreetingDarkPreview() {
     WellnessTheme(darkTheme = true) {
-        DayCard()
+        DayCard(
+            Day(
+                R.string.day_2,
+                R.string.description_2,
+                R.drawable.unsplash_2,
+            )
+        )
     }
 }
